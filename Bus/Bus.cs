4b9d55c10fc67848a20a431965b9bcde532dece2
iamHolo <nneketaa@yandex.ru>
2020-12-04 17:46:38 +0500

    Добавьте файлы проекта.

---
+++ Bus/Bus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace Bus
+{
+    class Bus
+    {
+        public string name;
+        public string secondName;
+        public string brand;
+        public string year;
+        public string numberOfBus;
+        public int numberOfRoute;
+        public int millage;
+
+        public static int count=0;
+
+        //public static List<string> buses = new List<string>();
+
+        public static string[,] buses = new string[100,100];
+
+        public Bus(string name, string secondName, string brand, string year, string numberOfBus, int numberOfRoute, int millage)
+        {
+            buses[count, 0] = name;
+            buses[count, 1] = secondName;
+            buses[count, 2] = brand;
+            buses[count, 3] = year;
+            buses[count, 4] = numberOfBus;
+            buses[count, 5] = numberOfRoute.ToString();
+            buses[count, 6] = millage.ToString();
+            count++;
+        }
+    }
+}
---
+++ Bus/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus b1 = new Bus("Никита", "Беликов", "BMW", "1990", "a192i", 14, 90000);
+            Bus b2 = new Bus("Кирилл", "Чухарев", "BMW", "1999", "fg1452i", 14, 50000);
+            Bus b3 = new Bus("Андрей", "Галеев", "Камаз", "2005", "6kt192i", 2, 40000);
+            Bus b4 = new Bus("Данил", "Данилов", "Chevrolet", "1980", "46g2i", 10, 5000);
+            Bus b5 = new Bus("Эдик", "Бонадрев", "Lada", "2010", "a112i", 15, 0);
+
+
+            Console.WriteLine("Введите номер искомого маршрута :");
+            string numberOfRoute = Console.ReadLine();
+            Console.WriteLine("\nАвтобусы:\n");
+            for (int i = 0; i < Bus.count; i++)
+            {
+                if (numberOfRoute == Bus.buses[i, 5])
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.Write(Bus.buses[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }     
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Введите срок в годах, чтобы узнать, какие автобусы эксплуатируются дольше :");
+            string term = Console.ReadLine();
+            Console.WriteLine("\nАвтобусы:\n");
+            for (int i = 0; i < Bus.count; i++)
+            {
+                int expTerm = 2020 - Int32.Parse(Bus.buses[i, 3]);
+                if (expTerm > Int32.Parse(term))
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.Write(Bus.buses[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }    
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Введите пробег, чтобы узнать, у какого автобуса больше :");
+            string millage = Console.ReadLine();
+            Console.WriteLine("\nАвтобусы:\n");
+            for (int i = 0; i < Bus.count; i++)
+            {
+                if (int.Parse(millage) < int.Parse(Bus.buses[i, 6]))
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.Write(Bus.buses[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }   
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
